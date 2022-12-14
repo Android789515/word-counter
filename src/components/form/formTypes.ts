9e0708c1192ae5781f12a8b21585928e77426c63
Android789515 <derflug@mailfence.com>
2022-12-14 16:36:45 -0600

    Reverted export of the NewFormText type.

--- src/components/form/formTypes.ts
+++ src/components/form/formTypes.ts
@@ -1,4 +1,4 @@
 export type FormText = string;
-export type NewFormText = FormText;
 
+type NewFormText = FormText;
 export type updateFormText = (newFormText: NewFormText) => void; // Does state updates
