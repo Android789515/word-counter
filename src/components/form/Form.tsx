d3fa14e6cb045aafe24ddac6d05613493fc8051f
Android789515 <derflug@mailfence.com>
2022-12-15 18:28:38 -0600

    Hooked up custom and default styles to the Form component.

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,17 +1,27 @@
-import type { LabelText, FormText, updateFormText } from './formTypes';
+import type { FormClasses, LabelText, FormText, updateFormText } from './formTypes';
+
+import styles from './Form.module.scss';
 
 interface Props {
+    styleClasses?: FormClasses;
     labelText?: LabelText;
     formText: FormText;
     updateFormText: updateFormText;
 }
 
-const Form = ({ labelText, formText, updateFormText }: Props) => {
+const Form = ({ styleClasses, labelText, formText, updateFormText }: Props) => {
+    // Extracted for readability
+    const layout = styleClasses?.layoutClass || styles.formLayoutDefaults;
+
     return (
-        <label>
+        <label className={layout}>
             {labelText}
 
             <textarea
+                className={`
+                    ${styles.formDefaults}
+                    ${styleClasses?.textFieldClass}
+                `}
                 value={formText}
                 onChange={event => updateFormText(event.target.value)}
             />
