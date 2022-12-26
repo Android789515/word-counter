0dc1a769cb2152abb7b08464af2a47585d4db98f
Android789515 <derflug@mailfence.com>
2022-12-26 12:34:54 -0600

    Vastly simplified the Metrics feature.
    Removed the need for the InfoGroup component and simplified the convoluted logic around rendering the metrics.

--- src/features/metrics/Metrics.tsx
+++ src/features/metrics/Metrics.tsx
@@ -1,22 +1,29 @@
-import { Info, InfoGroup } from 'components/info-group';
+import { Number, String } from 'utils/primitives';
+
+import type { MetricsData } from './metricTypes';
 import { Metric } from './metric/Metric';
 
 import styles from './Metrics.module.scss';
 
 interface Props {
-    metrics: Info[]
+    metrics: MetricsData;
 }
 
 export const Metrics = ({ metrics }: Props) => {
+    const MetricComponents = Object.entries(metrics).map((metric, index) => {
+        const [ description, value ] = metric.map(Number.toString).map(String.capitalize);
+
+        return (
+            <Metric
+                key={index}
+                metric={{ description, value }}
+            />
+        );
+    });
+
     return (
-        <InfoGroup
-            styleClass={styles.metrics}
-            infoToDisplay={metrics}
-            renderInfoComponent={metric => (
-                <Metric
-                    metric={metric}
-                />
-            )}
-        />
+        <ul className={styles.metrics}>
+            {MetricComponents}
+        </ul>
     );
 };
