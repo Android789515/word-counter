c38a23342cbfe6595564dbe349bfe0ebff63123b
Android789515 <derflug@mailfence.com>
2022-12-26 12:33:15 -0600

    Refactored the primitives utility.
    Replaced the unused trim method for strings with a capitalize method and added a basic toString method for numbers that does not support radixes.

--- src/utils/primitives.ts
+++ src/utils/primitives.ts
@@ -1,5 +1,14 @@
 export const String = {
-    trim(string: string) {
-        return string.trim();
+    capitalize(string: string) {
+        const capitalizedFirstLetter = string[0].toUpperCase();
+
+        return capitalizedFirstLetter + string.slice(1);
+    }
+}
+
+export const Number = {
+    toString(number: number) {
+        // Radix not supported.
+        return number.toString();
     }
 }
