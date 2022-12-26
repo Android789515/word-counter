ba629a316ce2c3af9b7e60dd9e101986667057b5
Android789515 <derflug@mailfence.com>
2022-12-26 13:22:48 -0600

    Rendered the features in the app and hooked up the state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+
+import type { FormText } from 'components/form';
+import { Layout } from 'components/layout';
+import { getMetrics, Metrics } from 'features/metrics';
+import { TextEnter } from 'features/text-enter';
+
 import styles from './App.module.scss';
 
 const App = () => {
+    const [ userInput, updateUserInput ] = useState<FormText>('');
+
     return (
         <div className={styles.app}>
+            <Layout>
+                <Metrics
+                    metrics={getMetrics(userInput)}
+                />
 
+                <TextEnter
+                    userInput={userInput}
+                    updateUserInput={updateUserInput}
+                />
+            </Layout>
         </div>
     );
 };
