c7159abf012c2ab20568e7920495abc73f77f452
Android789515 <derflug@mailfence.com>
2022-12-28 19:32:10 -0600

    Create hook for hiding/showing the git checks
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/features/git-checks/useToggleShow.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const useToggleShow = (startShown?: boolean) => {
+    const [ isShown, setIsShown ] = useState(startShown);
+
+    const toggleShown = () => setIsShown(wasShown => !wasShown);
+
+    return {
+        isShown,
+        toggleShown
+    };
+};
+
+export default useToggleShow;
