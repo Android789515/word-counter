f8716b2619ce52a90b6af96729006a0702469d35
Android789515 <derflug@mailfence.com>
2022-12-30 14:18:57 -0600

    Give the git checks button a dark theme variant

--- src/features/git-checks/components/check-message-button/CheckMessageButton.tsx
+++ src/features/git-checks/components/check-message-button/CheckMessageButton.tsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { themeContext } from 'providers/theme-provider';
+
 import styles from './CheckMessageButton.module.scss';
 
 interface Props {
@@ -5,9 +9,14 @@ interface Props {
 }
 
 export const CheckMessageButton = ({ onClick }: Props) => {
+    const { isDarkTheme } = useContext(themeContext);
+
     return (
         <button
-            className={styles.checkMessageButton}
+            className={`
+                ${styles.checkMessageButton}
+                ${isDarkTheme() ? styles.checkMessageButtonDark : ''}
+            `}
             onClick={onClick}
         >
             Git-Check
