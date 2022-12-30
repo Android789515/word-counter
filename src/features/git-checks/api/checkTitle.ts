bb145b78d0a6a3034bca1b4697830e866fe5d26d
Android789515 <derflug@mailfence.com>
2022-12-29 19:06:00 -0600

    Move git title checks to its own file
    Decomposed the logic in the git checks api and in the check title function.
    
    Signed-off-by: Android789515 <derflug@mailfence.com>
    
    squash! Move git title checks to its own file
    
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/features/git-checks/api/checkTitle.ts
@@ -0,0 +1,54 @@
+import type { FormText } from 'components/form';
+import { MessageHeaderTypes } from '../gitCheckTypes';
+import { Bool, Str } from 'utils/primitives';
+
+const doesPassInitialChecks = (titleLine: string) => {
+    const maxTitleLength = 50;
+
+    return Object.values({
+        isNotEmpty: Str.isNotEmpty(titleLine),
+        hasNoPeriod: titleLine.at(-1)! !== '.',
+
+        is50LettersOrFewer: titleLine.length <= maxTitleLength,
+        startsWithLetter: /[a-zA-Z]/.test(titleLine.charAt(0))
+    }).every(Bool.doesPass);
+};
+
+const doesPassBasicChecks = (titleLine: string) => {
+    return Object.values({
+        isCapitalized: Str.isCapitalized(titleLine),
+    }).every(Bool.doesPass);
+};
+
+const doesPassConventionChecks = (titleLine: string) => {
+    return Object.values(MessageHeaderTypes).some(type => {
+        const hasValidHeaderType = titleLine.startsWith(type);
+
+        if (hasValidHeaderType) {
+            const [ , restOfTitle ] = titleLine.split(type);
+            const [ scope, summary ] = restOfTitle.split(':');
+
+            if (summary) {
+                // Removes the parenthesis if there is no scope
+                const convention = `${scope}: ${summary.trim()}`.replace('()', '');
+
+                return restOfTitle === convention;
+            }
+        }
+
+        return false;
+    });
+};
+
+export const checkTitle = (userInput: FormText) => {
+    const titleLine = userInput.split('\n').at(0)!;
+
+    if (doesPassInitialChecks(titleLine)) {
+        return (
+            doesPassBasicChecks(titleLine)
+            || doesPassConventionChecks(titleLine)
+        );
+    }
+
+    return false;
+};
